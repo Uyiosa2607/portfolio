d4895f4cc8bd52411244e0044484721afd3c9738
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2024-04-25 12:32:11 +0100

    mock project page added

---
+++ src/app/my-projects/page.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout/Layout"
+import "./projects.scss"
+
+export default function Projects(){
+    return (
+        <Layout>
+            <main className="projects">
+                <h4 className="page-info">Page is under contruction</h4>
+            </main>
+        </Layout>
+    )
+}
